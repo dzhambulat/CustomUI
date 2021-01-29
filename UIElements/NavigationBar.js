dea7755e1165c424bc8a657007047eecc17d0951
Dzhambulat Khasaev <dzkhasayev@gmail.com>
2021-01-30 00:01:09 +0300

    modify buttons

--- UIElements/NavigationBar.js
+++ UIElements/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../scenes/Home';
@@ -10,11 +10,82 @@ const NavBar = () =>{
     return (
         <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} options={{
+            tabBarLabel:"",
+                    tabBarButton: ({ focused }) => {
+                        return (
+                          <View
+        style={{
+        
+          bottom: 0,
+          height: 58,
+          marginLeft:50,
+          width: 58,
+          borderRadius: 3,
+          backgroundColor: '#5a95ff',
+          justifyContent: 'center',
+          alignItems: 'center',
+          shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 2,
+},
+shadowOpacity: 0.23,
+shadowRadius: 2.62,
+
+elevation: 4,
+
+        }}>
+      </View>
+                        )
+                    }
+                }} />
+                   <Tab.Screen name="Settings" component={SettingsScreen} options={{
+            tabBarLabel:"",
+                    tabBarButton: ({ focused }) => {
+                        return (
+                          <View
+        style={{
+          marginLeft:50,
+          bottom: 0,
+          height: 58,
+          width: 58,
+          borderRadius: 3,
+          backgroundColor: '#5a95ff',
+          justifyContent: 'center',
+          alignItems: 'center',
+          shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 2,
+},
+shadowOpacity: 0.23,
+shadowRadius: 2.62,
+
+elevation: 4,
+
+        }}>
+      </View>
+                        )
+                    }
+                }} />
         </Tab.Navigator>
       </NavigationContainer>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent:"center",
+    backgroundColor: '#ccc',
+    alignItems: 'center',   
+    justifyContent: 'center'
+  },
+  button: {
+    borderColor: "#000000",
+    borderWidth: 2
+  }
+});
 
 export default NavBar;
